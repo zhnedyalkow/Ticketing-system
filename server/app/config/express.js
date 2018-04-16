d1834fbf15adce99e0dc20b40a4a3d221eac4da0
DESKTOP-UTDKDOP\zhi <zh.nedyalkow@gmail.com>
2018-04-16 16:24:04 +0300

    Implemented express.js and merge

---
+++ server/app/config/express.js
@@ -0,0 +1,30 @@
+/* globals __dirname */
+
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const init = (init) => {
+    // defensive programming
+    if (typeof app.use !== 'function' || 
+        typeof app.set !== 'function') {
+        throw new Error('Invalid app');
+    }
+
+    // decorator 
+    app.use(bodyParser.urlencoded({
+        extended: true,
+    }));
+
+    // decorator
+    app.use('/static', express.static(path.join(__dirname, '../../public')));
+
+    // decorator
+    app.use(morgan('combined'));
+};
+
+module.exports = {
+    init,
+  };
