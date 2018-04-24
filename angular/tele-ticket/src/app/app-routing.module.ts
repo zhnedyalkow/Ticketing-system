1f13f6fa64aecc045b0e0999cb1e922bcccd88ca
DESKTOP-UTDKDOP\zhi <zh.nedyalkow@gmail.com>
2018-04-24 21:50:53 +0300

    pull

--- angular/tele-ticket/src/app/app-routing.module.ts
+++ angular/tele-ticket/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/auth' },
+//   { path: '', redirectTo: '/auth' },
   { path: 'auth', loadChildren: './auth/auth.module#AuthModule'},
   { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule'}
 ]
