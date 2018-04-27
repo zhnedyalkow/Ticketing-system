b569fca9789e8b27cfd34ac8dc169c056dfc845b
DESKTOP-UTDKDOP\zhi <zh.nedyalkow@gmail.com>
2018-04-27 17:38:05 +0300

    Implemented log out functionality.

--- angular/tele-ticket/src/app/dashboard/dashboard.component.ts
+++ angular/tele-ticket/src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { UserInfo } from '../models/users/user.info';
-import { Team } from '../models/teams/team';
 import { Subscription } from 'rxjs/Subscription';
 import { Observable } from 'rxjs/Observable';
+import { Router } from '@angular/router';
+
 import { DashboardService } from './shared/services/dashboard.service';
+import { Team } from '../models/teams/team';
+import { UserInfo } from '../models/users/user.info';
+import { AuthService } from '../core/authentication/auth.service';
 
 
 @Component({
@@ -14,9 +17,13 @@ import { DashboardService } from './shared/services/dashboard.service';
 export class DashboardComponent implements OnInit {
     userInfo$: Observable<Object>;
 
-    constructor(private dashService: DashboardService) { }
+    constructor(private router: Router, private dashService: DashboardService, private auth: AuthService) { }
     ngOnInit() {
         this.userInfo$ = this.dashService.getUserInfo();
     }
 
+    logoutUser() {
+        this.auth.logout();
+        this.router.navigate(['./']);
+    }
 }
